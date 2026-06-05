a034e68c302b425882d88327a909bf142e318a05
smit <smit59349@gmail.com>
2026-06-05 21:58:34 +0530

    Add Login API

--- backend/node.js/index.js
+++ backend/node.js/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const User = require('./db/user');
+const user = new User(req.body);
 require('./db/config');
 require('./db/user');
 const path = require('path');
@@ -9,9 +9,29 @@ app.use(express.json());
 app.use(cors());
 
 
-app.post("/registration",async (req,res)=>{
-    const user = user_model(req.body);
+app.post('/registration',async (req,res)=>{
+    const user = new User(req.body);
     const result = await user.save();
     res.send(result);
 });
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email, password });
+
+  if (user) {
+    res.send({
+      success: true,
+      message: "Login Successful",
+      user,
+    });
+  } else {
+    res.send({
+      success: false,
+      message: "Invalid Email or Password",
+    });
+  }
+});
+
 app.listen(8080);
